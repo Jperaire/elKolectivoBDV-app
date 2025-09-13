c3ea88ca23f22f80a39e58b301a846282a23eb49
Julen <julenperaire@gmail.com>
2025-09-13 17:41:07 +0200

    style: fonts sizes

--- src/pages/admin/AdminDashboard/index.tsx
+++ src/pages/admin/AdminDashboard/index.tsx
@@ -6,31 +6,29 @@ import styles from "./AdminDashboard.module.css";
 
 export const AdminDashboard = () => {
     return (
-        <div className="page">
+        <Card className="page">
             <h1 className="h1">Admin Dashboard</h1>
 
-            <Card>
-                <section className={styles.cards}>
-                    {adminActionsData.map((action) => (
-                        <Link
-                            key={action.link}
-                            to={action.link}
-                            className={styles.bubble}
-                            aria-label={action.title}
-                        >
-                            {action.img && (
-                                <img
-                                    src={action.img}
-                                    alt=""
-                                    aria-hidden="true"
-                                    className={styles.icon}
-                                />
-                            )}
-                            <h2 className={styles.title}>{action.title}</h2>
-                        </Link>
-                    ))}
-                </section>
-            </Card>
-        </div>
+            <section className={styles.cards}>
+                {adminActionsData.map((action) => (
+                    <Link
+                        key={action.link}
+                        to={action.link}
+                        className={styles.bubble}
+                        aria-label={action.title}
+                    >
+                        {action.img && (
+                            <img
+                                src={action.img}
+                                alt=""
+                                aria-hidden="true"
+                                className={styles.icon}
+                            />
+                        )}
+                        <h2 className={styles.title}>{action.title}</h2>
+                    </Link>
+                ))}
+            </section>
+        </Card>
     );
 };
